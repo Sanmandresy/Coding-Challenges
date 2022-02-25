e566fb861da98b479b2779f2b3ad7b5790d12bec
Mahefa Ny Anjara <98638690+Mahefaa@users.noreply.github.com>
2022-02-25 10:52:00 +0100

    fix: run issues

--- Battledev Yoghurts/battledev1(2020).js
+++ Battledev Yoghurts/battledev1(2020).js
@@ -4,7 +4,7 @@ let find2Maxes=(survey)=>{ //a function to find the two  most "voted" properties
     let firstNum=0; //first's votes
     let second=``;
     let secondNum=0;
-    for(colors in survey){
+    for(let colors in survey){
         if(survey[colors]>firstNum){
             if(firstNum!=0){
                 //top two takes firstNum's value before it changes
@@ -14,7 +14,7 @@ let find2Maxes=(survey)=>{ //a function to find the two  most "voted" properties
             first=colors;
             firstNum=survey[colors];
         }
-        if(topTwo==0 && survey[colors]<=firstNum && survey[colors]>secondNum && colors!=first){
+        if(second==0 && survey[colors]<=firstNum && survey[colors]>secondNum && colors!=first){
         //if top one never changes, we have to get the top two
             secondNum=survey[colors];
             second=colors;
@@ -26,7 +26,7 @@ let find2Maxes=(survey)=>{ //a function to find the two  most "voted" properties
 let mySurvey=(table)=>{
     //here datas are put in a table
     let colorSurvey={};
-    for(color of table){
+    for(let color of table){
         if(colorSurvey.hasOwnProperty(color)){ //we verify if the property color exists in our object
             colorSurvey[color]++;
         }
@@ -35,7 +35,7 @@ let mySurvey=(table)=>{
             colorSurvey[color]=1;
         }
     }
-    console.log(`Here's the poll result: ${find_2_maxes(colorSurvey)}`);
+    return`Here's the poll result: ${find2Maxes(colorSurvey)}`
 }
 let table=['red','red','red','green','green','green','green','red']
-create_voted_poll(table);
+mySurvey(table);
